aff453ee82713c480a7346c49a9de4b39224ddb0
dtcokr <53829474+dtcokr@users.noreply.github.com>
2021-03-17 21:49:27 +0800

    Update plugin.go

--- plugin.go
+++ plugin.go
@@ -6,14 +6,6 @@ import (
 		// plug in plugins here, for example:
 		// _ "import/path/here"
 
-		// http.ipfilter
-		_ "github.com/pyed/ipfilter"
-		// http.git
-		_ "github.com/abiosoft/caddy-git"
-		// http.nobots
-		_ "github.com/Xumeiquer/nobots"
-		// http.realip
-		_ "github.com/captncraig/caddy-realip"
 		// caddy-docker-proxy
 		_ "github.com/lucaslorentz/caddy-docker-proxy/plugin"
 )
